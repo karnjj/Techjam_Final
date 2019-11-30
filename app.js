535bd6c413edbea27a49c5c75a1b1961c466ac0d
Karnjj <karnjj@hotmail.com>
2019-11-30 18:59:33 +0700

    update feature F

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var logger = require('morgan');
 require('dotenv').config()
 var app = express()
 var PORT = process.env.PORT || 8000
-var DEC = process.env.DECIMAL_PLACES || 2
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
 	extended : true
@@ -13,10 +12,38 @@ app.use(logger('dev'));
 var fi_pos = {}
 var se_pos = {}
 var robot = {}
+var point = []
 var distance = function(a,b) {
 	var absX = Math.abs(a.x-b.x) 
 	var absY = Math.abs(a.y-b.y)
 	return Math.sqrt(absX*absX+absY*absY)
+}
+var closestpair = function(l,r) {
+	if(r-l+1 == 2) return distance(point[l],point[r])
+	if(r-l+1 == 1) return Number.MAX_SAFE_INTEGER
+	var T = (l + r)/2
+	var mn = Math.min(closestpair(l,T),closestpair(T,r))
+	var strip = []
+	for(var i = l; i <= r; i++) {
+		if(Math.abs(point[T].x-point[i].x) < mn) {
+		strip.push(point[i])
+		}
+	}
+	var sz = strip.length;
+	strip.sort((a,b) => {
+		if(a.y == b.y) {
+			if(a.x > b.x) return 1;
+			else return -1
+		}else if(a.y > b.y) return 1;
+		else return -1;
+	})
+	for(var i = 0; i < sz-1; i++) {
+		for(var j = i + 1; j < sz; j++) {
+		if(strip[j].y-strip[i].y >= mn) break;
+		mn = Math.min(distance(strip[i],strip[j]),mn);
+		}
+	}
+	return mn;
 }
 app.post('/distance', (req,res) => {
 	var data = req.body
@@ -56,22 +83,46 @@ app.get('/robot/:id/position',(req,res) => {
 app.post('/nearest',(req,res) => {
 	var data = req.body
 	var arr = []
+	var tmp = []
 	var ref = data.ref_position
-	var minn = Number.MAX_SAFE_INTEGER
-	var idx = 1000000
 	for(var e in robot) {
 		var dist  = distance(ref,robot[e])
 		//console.log(e  + "  " + dist);
-		if(minn > dist) {
-			minn = dist
-			idx = e
-		}else if(minn == dist && e < idx) idx = e
+		tmp.push({dist,e})
 	}
-	if(idx!=1000000)arr.push(Number(idx))
-	//console.log(idx);
+	tmp.sort((a,b) => {
+		if(a.dist == b.dist) {
+			if(a.e > b.e) return 1
+			else return -1
+		}else if(a.dist > b.dist) return 1
+		else return -1
+	})
+	if(data.k === undefined) data.k = 1
+	var n = Math.min(data.k,tmp.length)
+	for(var i = 0; i < n; i++) arr.push(Number(tmp[i].e)) 
+	//console.log(tmp);
 	res.json({
 		robot_ids : arr
 	})
+})
+app.get('/closestpair',(req,res) => {
+	point = [];
+	for(var e in robot) point.push(robot[e])
+	if(point.length < 2) {
+		res.status(424).send('')
+	}else {
+		point.sort((a,b) => {
+			if(a.x == b.x) {
+				if(a.y > b.y) return 1;
+				else return -1
+			}else if(a.x > b.x) return 1;
+			else return -1;
+		})
+		//console.log(point);
+		res.json({
+			distance: Number(closestpair(0,point.length-1).toFixed(3))
+		})
+	}
 })
 app.listen(PORT,() => {
 	console.log("Starting server at PORT " + PORT)
